32f78382e533df6cf5b73b3ff5690896432fcdfc
Ryan Cross <ryan.cross.home@gmail.com>
2022-06-13 14:56:33 -0500

    Update with config

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,14 +1,30 @@
 import { getDbConnectionInfo } from "@zero-tech/data-store-core/lib/database/helpers";
 import { Context } from "@azure/functions";
-import { MongoClient } from "mongodb";
+import { MongoClient, MongoClientOptions } from "mongodb";
 import { MongoDbService } from "@zero-tech/data-store-core/lib/database/mongo/mongoDbService";
 import * as env from "env-var"
 
+const dbUser = env.get("DATABASE_USERNAME").required().asString();
+const dbPassword = env.get("DATABASE_PASSWORD").required().asString();
 const dbUri = env.get("DATABASE_URI").required().asString();
 
 export async function getConnectedDbClient(context: Context) {
-  const dbClient = new MongoClient(dbUri);
+
+  const options: MongoClientOptions = {
+    connectTimeoutMS: 10000,
+    retryWrites: true,
+    retryReads: true,
+    w: "majority",
+    keepAlive: true,
+    auth: {
+      username: dbUser,
+      password: dbPassword
+    }
+  };
+
+  const dbClient = new MongoClient(dbUri, options);
   await dbClient.connect();
+  console.log("Database connection established");
 
   return dbClient;
 }

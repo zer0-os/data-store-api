566033e93fd1f21a5ab1c1633bd672a64243915c
Ryan Cross <ryan.cross.home@gmail.com>
2023-01-08 16:22:15 -0600

    Name filtering for subdomain endpoints / Remove legacy endpoint code (#33)
    * upgrade pkg
    
    * remove optionality of findOptions by the time DSS call is reached

--- domain-get-subdomains-deep/index.ts
+++ domain-get-subdomains-deep/index.ts
@@ -1,5 +1,5 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import { DomainId } from "@zero-tech/data-store-core";
+import { DomainId, Maybe } from "@zero-tech/data-store-core";
 import { getMongoDomainService } from "../src/helpers";
 import { getDomainFindOptionsFromQuery } from "../src/helpers/domainFindOptionsHelper";
 import { validateDomainId } from "../src/schemas";
@@ -8,6 +8,7 @@ import { createErrorResponse } from "../src/helpers/createErrorResponse";
 import { generatePaginationResponse } from "../src/helpers/paginationHelper";
 import * as constants from "../src/constants";
 import { DomainDto } from "../src/types";
+import { checkFilterCompatibility } from "../src/helpers/filterHelper";
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
@@ -22,10 +23,14 @@ const httpTrigger: AzureFunction = async function (
     }
 
     const findOptions = getDomainFindOptionsFromQuery(req, true);
+    const nameFilter: Maybe<string> = req.query.name;
+    checkFilterCompatibility(findOptions, nameFilter, "name");
+    
     const domainService = await getMongoDomainService(context.log);
     const response = await domainService.getSubdomainsDeep(
       req.params.id,
-      findOptions
+      findOptions,
+      nameFilter
     );
     const paginationResponse = generatePaginationResponse<DomainDto>(
       response,
